b21ab995c8ef730db2cb62013f32ff3d394ca614
Zhi Ya'an <2651249307@qq.com>
2024-11-01 00:40:29 +0800

    LC测试函数编写，未验证正确性

--- TiveQP/IndexBuilding/LocationCode_test.go
+++ TiveQP/IndexBuilding/LocationCode_test.go
@@ -1,1 +1,14 @@
 package indexbuilding
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLocationEncoding(t *testing.T) {
+	cityName := "ATLANTA"
+	lat := 33.7660237
+	lng := -84.5301237
+	locationCode, _ := LocationEncoding(cityName, lat, lng)
+	fmt.Println(locationCode)
+}
