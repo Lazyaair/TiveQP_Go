5d5f55ee3b3e39e251118e582b2d776acae66e34
Zhi Ya'an <2651249307@qq.com>
2024-11-01 16:46:55 +0800

    编码补集及测试，正确性未验证

--- TiveQP/IndexBuilding/LocationCode.go
+++ TiveQP/IndexBuilding/LocationCode.go
@@ -105,3 +105,96 @@ func LocationEncoding(cityName string, lat, lng float64) ([]string, error) {
 		return nil, fmt.Errorf("经纬度编号有误！")
 	}
 }
+
+func LocationEncodingComplement(cityName string, lat, lng float64) ([]string, error) {
+	cityIndex := GetCityIndex(cityName)
+	cityBoundary := GetCityLatLng(cityIndex)
+
+	// 经纬度投影编号
+	num_lat := Projection(cityBoundary[0], cityBoundary[1], lat)
+	num_lng := Projection(cityBoundary[2], cityBoundary[3], lng)
+
+	// 网格编号
+	place_index := num_lng*splitCount + num_lat
+
+	result := make([]string, 0, 6)
+	switch {
+	case num_lat == 0 && num_lng == 0:
+		// 左下角
+		n1, _ := Range(bitsize, place_index+2, place_index+splitCount-1)
+		n2, _ := Range(bitsize, place_index+splitCount+2, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		return result, nil
+	case num_lat == 0 && num_lng == splitCount-1:
+		// 左上角
+		n1, _ := Range(bitsize, 0, place_index-splitCount-1)
+		n2, _ := Range(bitsize, place_index-splitCount+2, place_index-1)
+		n3, _ := Range(bitsize, place_index+2, splitCount*splitCount)
+		result = append(result, n2...)
+		result = append(result, n1...)
+		result = append(result, n3...)
+		return result, nil
+	case num_lat == splitCount-1 && num_lng == 0:
+		// 右下角
+		n1, _ := Range(bitsize, 0, place_index-2)
+		n2, _ := Range(bitsize, place_index+1, place_index+splitCount-2)
+		n3, _ := Range(bitsize, place_index+splitCount+1, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		result = append(result, n3...)
+		return result, nil
+	case num_lat == splitCount-1 && num_lng == splitCount-1:
+		// 右上角
+		n1, _ := Range(bitsize, 0, place_index-splitCount-2)
+		n2, _ := Range(bitsize, place_index-splitCount+1, place_index-2)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		return result, nil
+	case num_lat == 0 && num_lng > 0 && num_lng < splitCount-1:
+		// 左 边
+		n1, _ := Range(bitsize, 0, place_index-1)
+		n2, _ := Range(bitsize, place_index+2, place_index+splitCount-1)
+		n3, _ := Range(bitsize, place_index+splitCount+2, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		result = append(result, n3...)
+		return result, nil
+	case num_lat == splitCount-1 && 0 < num_lng && num_lng < splitCount-1:
+		// 右 边
+		n1, _ := Range(bitsize, 0, place_index-splitCount-2)
+		n2, _ := Range(bitsize, place_index-splitCount+1, place_index-2)
+		n3, _ := Range(bitsize, place_index+1, place_index+splitCount-2)
+		n4, _ := Range(bitsize, place_index+splitCount+1, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		result = append(result, n3...)
+		result = append(result, n4...)
+		return result, nil
+	case 0 < num_lat && num_lat < splitCount-1 && num_lng == 0:
+		// 下 边
+		n1, _ := Range(bitsize, 0, place_index-2)
+		n2, _ := Range(bitsize, place_index+2, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		return result, nil
+	case 0 < num_lat && num_lat < splitCount-1 && num_lng == splitCount-1:
+		// 上 边
+		n1, _ := Range(bitsize, 0, place_index-2)
+		n2, _ := Range(bitsize, place_index+2, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		return result, nil
+	case 0 < num_lat && num_lat < splitCount-1 && 0 < num_lng && num_lng < splitCount-1:
+		// 中 间
+		n1, _ := Range(bitsize, 0, place_index-2)
+		n2, _ := Range(bitsize, place_index+2, place_index+splitCount-2)
+		n3, _ := Range(bitsize, place_index+splitCount+2, splitCount*splitCount)
+		result = append(result, n1...)
+		result = append(result, n2...)
+		result = append(result, n3...)
+		return result, nil
+	default:
+		return nil, fmt.Errorf("经纬度编号有误！")
+	}
+}
--- TiveQP/IndexBuilding/LocationCode_test.go
+++ TiveQP/IndexBuilding/LocationCode_test.go
@@ -10,5 +10,7 @@ func TestLocationEncoding(t *testing.T) {
 	lat := 33.7660237
 	lng := -84.5301237
 	locationCode, _ := LocationEncoding(cityName, lat, lng)
-	fmt.Println(locationCode)
+	fmt.Println("Code:", locationCode)
+	locationCodeComplement, _ := LocationEncodingComplement(cityName, lat, lng)
+	fmt.Println("ComplementCode:", locationCodeComplement)
 }
