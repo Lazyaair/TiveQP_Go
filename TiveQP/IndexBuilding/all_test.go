9f7be5eee72a8d5f53e4029ef9fa3aa0c1ab85b5
Zhi Ya'an <2651249307@qq.com>
2024-11-02 13:39:59 +0800

    调整目录结构

---
+++ TiveQP/IndexBuilding/all_test.go
@@ -0,0 +1,71 @@
+package indexbuilding
+
+import (
+	"fmt"
+	"testing"
+)
+
+// city test
+func TestCity(t *testing.T) {
+	fmt.Println(GetCityIndex("ALAFAYA"))                    // 输出 2
+	fmt.Println(GetCityIndex("51-RICHARD-BEALL-HWY-17-92")) // 输出 0
+	fmt.Println(GetCityLatLng(2))
+}
+
+// element test
+func TestPrefix(t *testing.T) {
+	for i := 24; i < 47; i++ {
+		fmt.Println(Prefix(6, i))
+	}
+}
+
+// func TestSamesize(t *testing.T) {
+// 	fmt.Println(Samesize("00101110011", "0010111001*"))
+// }
+
+func TestRange(t *testing.T) {
+	fmt.Println(Range(6, 16, 16))
+}
+
+// location test
+func TestLocationEncoding(t *testing.T) {
+	cityName := "ATLANTA"
+	lat := 33.7660237
+	lng := -84.5301237
+	locationCode, _ := LocationEncoding(cityName, lat, lng)
+	fmt.Println("Code:", locationCode)
+	locationCodeComplement, _ := LocationEncodingComplement(cityName, lat, lng)
+	fmt.Println("ComplementCode:", locationCodeComplement)
+}
+
+// time test
+func TestTimePointEncoding(t *testing.T) {
+	fmt.Println(TimePointEncoding(12, 11))
+}
+
+func TestTimeRangeEncoding(t *testing.T) {
+	fmt.Println(TimeRangeEncoding(8, 0, 9, 0))
+}
+
+func TestTimeRangeEncodingComplement(t *testing.T) {
+	fmt.Println(TimeRangeEncodingComplement(8, 48, 8, 50))
+}
+
+// type test
+func TestGetTypeIndex(t *testing.T) {
+	fmt.Println(GetTypeIndex("Financial Services")) // 1
+	fmt.Println(GetTypeIndex("Medical Supplies"))   // 2
+}
+
+func TestTypeEncoding(t *testing.T) {
+	fmt.Println(GetTypeIndex("Dentists"))
+	fmt.Println(TypeEncoding("Dentists"))
+}
+
+func TestTypeEncodingComplement(t *testing.T) {
+	fmt.Println(TypeEncodingComplement("Dentists"))
+}
+
+// user test
+
+// owner test
