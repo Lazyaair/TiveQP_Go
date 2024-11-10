51035318491254557b9646bfcfdacfd0eef35565
Zhi Ya'an <2651249307@qq.com>
2024-11-10 16:36:11 +0800

    调整

--- TiveQP/main.go
+++ TiveQP/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	indexbuilding "TiveQP/IndexBuilding"
-	"fmt"
-)
-
 func main() {
-	fmt.Println(indexbuilding.GetCityIndex("ALAFAYA")) // 输出 2
-	fmt.Println(indexbuilding.GetCityLatLng(2))        // 输出 -1
+
 }
