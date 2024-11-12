2d440eee6ce786051a0978bc156371c7a8a77df2
Zhi Ya'an <2651249307@qq.com>
2024-11-12 23:45:47 +0800

    节点属性定义

--- TiveQP/Construction/Node.go
+++ TiveQP/Construction/Node.go
@@ -1,12 +1,40 @@
 package construction
 
-// LeafNode 表示下层树的节点
-type LeafNode struct {
-	Value int
-	Left  *LeafNode
-	Right *LeafNode
+// DownNode 表示下层树的节点
+type DownNode struct {
+	Data []string
+	LCS  []string
+	TCS  []string
+	// IBF的插入位置，HMAC==>bigint=(mod)=>index+'|'+0/1
+	Bits_LCS [][]string
+	Bits_TCS [][]string
+	// Bits的摘要
+	HV_LCS [][]byte
+	HV_TCS [][]byte
+	// 原始数据的摘要
+	HV []byte
+	// 子节点
+	Left  *DownNode
+	Right *DownNode
 }
 
-// TypeNode 表示上层树的节点
-type TypeNode struct {
+// UpNode 表示上层树的节点
+type UpNode struct {
+	Data []string
+	LCS  []string
+	TCS  []string
+	YCS  []string
+	// IBF的插入位置，HMAC==>bigint=(mod)=>index+'|'+0/1
+	Bits_LCS [][]string
+	Bits_TCS [][]string
+	Bits_YCS [][]string
+	// Bits的摘要
+	HV_LCS [][]byte
+	HV_TCS [][]byte
+	HV_YCS [][]byte
+	// 子节点的合并摘要
+	HV []byte
+	// 子节点
+	Left  *UpNode
+	Right *UpNode
 }
