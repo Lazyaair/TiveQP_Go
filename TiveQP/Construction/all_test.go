01329c2ef8cf9187fcb4769d124d2bc9048a1dd7
Zhi Ya'an <2651249307@qq.com>
2024-11-16 13:33:29 +0800

    建树完成（20k数据），仅建树，未保存IBF

--- TiveQP/Construction/all_test.go
+++ TiveQP/Construction/all_test.go
@@ -57,3 +57,53 @@ func TestLoadData(t *testing.T) {
 		fmt.Printf("%+v\n", *owner)
 	}
 }
+
+func TestBuildTree(t *testing.T) {
+	filename := "E:\\Github\\TiveQP\\TiveQP\\TiveQP\\Data\\20k.txt" // 文件名
+	owners, err := LoadOwners(filename)
+	if err != nil {
+		fmt.Println("加载 Owner 数据出错:", err)
+		return
+	}
+	ibfLength := 200000
+	Keylist := []string{"2938879577741549", "8729598049525437", "8418086888563864", "0128636306393258", "2942091695121238", "6518873307787549"}
+	rb := 235648
+
+	// 生成20个subroot节点
+	subroots, err := BuildTreesByChunks(owners, ibfLength, Keylist, rb)
+	if err != nil {
+		fmt.Println("Error building subroots:", err)
+	} else {
+		fmt.Println("Subroots built successfully!")
+	}
+
+	// 使用subroots构建最终的树
+	finalRoot, err := CreateFinalTree(subroots, ibfLength, Keylist, rb)
+	if err != nil {
+		fmt.Println("Error creating final tree:", err)
+	} else {
+		fmt.Println("Final tree created successfully!")
+	}
+	finalRoot.PreOrderTraversal()
+}
+
+func TestEncDec(t *testing.T) {
+	plaintext := []byte("Automotive**CHAMBLEE**33.896392**-84.303269**9**0**20**0")
+	key := []byte("2bc73dw20ebf4d46")
+
+	// 加密
+	ciphertext, err := Encrypt(plaintext, key)
+	if err != nil {
+		panic(err)
+	}
+	println("Encrypted Ciphertext:", ciphertext)
+
+	// 解密
+	decryptedText, err := Decrypt(ciphertext, key)
+	if err != nil {
+		panic(err)
+	}
+
+	// 打印解密后的明文
+	fmt.Println("Decrypted Text:", string(decryptedText))
+}
